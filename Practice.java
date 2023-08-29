723ce45cdab8f8101df2f89e2eb88154fb285622
AdamRazakk <139847585+AdamRazakk@users.noreply.github.com>
2023-08-29 16:26:00 +0800

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Practice {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JCheckBox DINEINCheckBox;
+    private JCheckBox TAKEAWAYCheckBox;
+    private JCheckBox CONFIRMORDERCheckBox;
+    private JButton PROCEEDButton;
+}
